0c9a293de44b03a3b64540d3fe57dec4f907830f
Ayub Islam <ayubislam863@gmail.com>
2025-01-17 02:52:05 +0600

    all user post fucntion create

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const app = express();
 const port = process.env.PORT || 7000;
 const cors = require("cors");
 
-
-app.use(cors({ origin: [] }));
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ds3da.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,23 +19,30 @@ const client = new MongoClient(uri, {
 	},
 });
 async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	try {
+		await client.connect();
+		const UserCollection = client.db("Blood_Donation").collection("all-users");
 
+		app.post("/all-users", async (req, res) => {
+			const users = req.body;
+			console.log(users);
+			
+			const result = await UserCollection.insertOne(users);
+			res.send(result);
+		});
 
-      
-    } finally {
-    //   // Ensures that the client will close when you finish/error
-    //   await client.close();
-    }
-  }
-  run().catch(console.dir);
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		//   // Ensures that the client will close when you finish/error
+		//   await client.close();
+	}
+}
+run().catch(console.dir);
 
-  app.get("/", async (req, res) => {
+app.get("/", async (req, res) => {
 	res.send("hello world");
 });
 
